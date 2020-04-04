70ce9735d42704660c2402de695cf3713e2af016
KalpasWang <48082968+KalpasWang@users.noreply.github.com>
2020-04-04 23:12:45 +0800

    test halfway

--- tests/Feature/PostToTimelineTest.php
+++ tests/Feature/PostToTimelineTest.php
@@ -6,18 +6,45 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\User;
+use App\Post;
 
 class PostToTimelineTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function a_user_can_post_a_text_post()
-    {
-      $this->actingAs($user = factory(User::class)->create(), 'api');
 
-      $response = $this->post('/api/posts', [])
-    }
+  use RefreshDatabase;
+
+  
+  public function testUserCanPostTextPost()
+  {
+    $this->withoutExceptionHandling();
+    $this->actingAs($user = factory(User::class)->create(), 'api');
+
+    $response = $this->post('/api/posts', [
+      'data' => [
+        'type' => 'posts',
+        'attributes' => [
+          'body' => 'Testing body'
+        ]
+      ]
+    ]);
+
+    $post = Post::first();
+
+    $this->assertCount(1, Post::all());
+    $this->assertEquals($user->id, $post->user_id);
+    $this->assertEquals('Testing Body', $post->body);
+    $response->assertStatus(201)
+      ->assertJson([
+        'data' => [
+          'type' => 'posts',
+          'post_id' => $post->id,
+          'attributes' => [
+            'body' => 'Testing Body',
+          ]
+        ],
+        'links' => [
+          'self' => url('/posts/'.$post->id),
+        ]
+      ]);
+  }
 }
