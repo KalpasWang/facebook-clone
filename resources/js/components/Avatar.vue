a72c7e9afad4e7d66ac9d67c3c573ba87cc4e456
KalpasWang <48082968+KalpasWang@users.noreply.github.com>
2020-04-23 22:24:27 +0800

    add navbar component

---
+++ resources/js/components/Avatar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-link :to="path" class="h-full flex justify-center">
+      <img src="https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg" alt="profile image for user" class="w-28px h-28px object-cover rounded-full">
+      <span class="ml-1 font-bold">{{ username }}</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['path', 'username']
+}
+</script>
+
+<style>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        "3/10": "30%",
+        "28px": "28px"
+      },
+      height: {
+        "28px": "28px"
+      }
+    },
   },
   variants: {
     outline: ['focus', 'responsive', 'hover'],
