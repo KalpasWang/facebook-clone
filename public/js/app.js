5d0e49735dea82af1bd96e8475956a5002984883
KalpasWang <48082968+KalpasWang@users.noreply.github.com>
2020-05-07 23:11:13 +0800

    post design

--- public/js/app.js
+++ public/js/app.js
@@ -331,6 +331,29 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({});
 
 /***/ }),
@@ -1353,44 +1376,257 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
+  return _c(
+    "div",
+    { staticClass: "bg-white rounded shadow w-full max-w-sm mt-6" },
+    [
+      _vm._m(0),
+      _vm._v(" "),
+      _vm._m(1),
+      _vm._v(" "),
+      _c(
+        "div",
+        { staticClass: "px-4 pt-2 flex justify-between text-gray-700 text-sm" },
+        [
+          _c("div", { staticClass: "flex" }, [
+            _c(
+              "svg",
+              {
+                attrs: {
+                  width: "18",
+                  height: "18",
+                  xmlns: "http://www.w3.org/2000/svg",
+                  "xmlns:xlink": "http://www.w3.org/1999/xlink",
+                  viewBox: "0 0 16 16"
+                }
+              },
+              [
+                _c(
+                  "defs",
+                  [
+                    _c(
+                      "linearGradient",
+                      {
+                        attrs: {
+                          id: "a",
+                          x1: "50%",
+                          x2: "50%",
+                          y1: "0%",
+                          y2: "100%"
+                        }
+                      },
+                      [
+                        _c("stop", {
+                          attrs: { offset: "0%", "stop-color": "#18AFFF" }
+                        }),
+                        _c("stop", {
+                          attrs: { offset: "100%", "stop-color": "#0062DF" }
+                        })
+                      ],
+                      1
+                    ),
+                    _c(
+                      "filter",
+                      {
+                        attrs: {
+                          id: "c",
+                          width: "118.8%",
+                          height: "118.8%",
+                          x: "-9.4%",
+                          y: "-9.4%",
+                          filterUnits: "objectBoundingBox"
+                        }
+                      },
+                      [
+                        _c("feGaussianBlur", {
+                          attrs: {
+                            in: "SourceAlpha",
+                            result: "shadowBlurInner1",
+                            stdDeviation: "1"
+                          }
+                        }),
+                        _c("feOffset", {
+                          attrs: {
+                            dy: "-1",
+                            in: "shadowBlurInner1",
+                            result: "shadowOffsetInner1"
+                          }
+                        }),
+                        _c("feComposite", {
+                          attrs: {
+                            in: "shadowOffsetInner1",
+                            in2: "SourceAlpha",
+                            k2: "-1",
+                            k3: "1",
+                            operator: "arithmetic",
+                            result: "shadowInnerInner1"
+                          }
+                        }),
+                        _c("feColorMatrix", {
+                          attrs: {
+                            in: "shadowInnerInner1",
+                            values:
+                              "0 0 0 0 0 0 0 0 0 0.299356041 0 0 0 0 0.681187726 0 0 0 0.3495684 0"
+                          }
+                        })
+                      ],
+                      1
+                    ),
+                    _c("path", {
+                      attrs: {
+                        id: "b",
+                        d: "M8 0a8 8 0 00-8 8 8 8 0 1016 0 8 8 0 00-8-8z"
+                      }
+                    })
+                  ],
+                  1
+                ),
+                _c("g", { attrs: { fill: "none" } }, [
+                  _c("use", { attrs: { fill: "url(#a)", "xlink:href": "#b" } }),
+                  _c("use", {
+                    attrs: {
+                      fill: "black",
+                      filter: "url(#c)",
+                      "xlink:href": "#b"
+                    }
+                  }),
+                  _c("path", {
+                    attrs: {
+                      fill: "white",
+                      d:
+                        "M12.162 7.338c.176.123.338.245.338.674 0 .43-.229.604-.474.725a.73.73 0 01.089.546c-.077.344-.392.611-.672.69.121.194.159.385.015.62-.185.295-.346.407-1.058.407H7.5c-.988 0-1.5-.546-1.5-1V7.665c0-1.23 1.467-2.275 1.467-3.13L7.361 3.47c-.005-.065.008-.224.058-.27.08-.079.301-.2.635-.2.218 0 .363.041.534.123.581.277.732.978.732 1.542 0 .271-.414 1.083-.47 1.364 0 0 .867-.192 1.879-.199 1.061-.006 1.749.19 1.749.842 0 .261-.219.523-.316.666zM3.6 7h.8a.6.6 0 01.6.6v3.8a.6.6 0 01-.6.6h-.8a.6.6 0 01-.6-.6V7.6a.6.6 0 01.6-.6z"
+                    }
+                  })
+                ])
+              ]
+            ),
+            _vm._v(" "),
+            _c("p", { staticClass: "ml-4" }, [_vm._v("502")])
+          ]),
+          _vm._v(" "),
+          _vm._m(2)
+        ]
+      ),
+      _vm._v(" "),
+      _c(
+        "div",
+        { staticClass: "flex justify-between border-1 border-gray-400 m-1" },
+        [
+          _c(
+            "button",
+            {
+              staticClass:
+                "flex justify-center py-1 rounded-lg text-sm text-gray-700 w-full hover:bg-gray-200"
+            },
+            [
+              _c(
+                "svg",
+                {
+                  staticClass: "fill-current w-5 h-5",
+                  attrs: {
+                    xmlns: "http://www.w3.org/2000/svg",
+                    viewBox: "0 0 24 24"
+                  }
+                },
+                [
+                  _c("path", {
+                    attrs: {
+                      d:
+                        "M20.8 15.6c.4-.5.6-1.1.6-1.7 0-.6-.3-1.1-.5-1.4.3-.7.4-1.7-.5-2.6-.7-.6-1.8-.9-3.4-.8-1.1.1-2 .3-2.1.3-.2 0-.4.1-.7.1 0-.3 0-.9.5-2.4.6-1.8.6-3.1-.1-4.1-.7-1-1.8-1-2.1-1-.3 0-.6.1-.8.4-.5.5-.4 1.5-.4 2-.4 1.5-2 5.1-3.3 6.1l-.1.1c-.4.4-.6.8-.8 1.2-.2-.1-.5-.2-.8-.2H3.7c-1 0-1.7.8-1.7 1.7v6.8c0 1 .8 1.7 1.7 1.7h2.5c.4 0 .7-.1 1-.3l1 .1c.2 0 2.8.4 5.6.3.5 0 1 .1 1.4.1.7 0 1.4-.1 1.9-.2 1.3-.3 2.2-.8 2.6-1.6.3-.6.3-1.2.3-1.6.8-.8 1-1.6.9-2.2.1-.3 0-.6-.1-.8zM3.7 20.7c-.3 0-.6-.3-.6-.6v-6.8c0-.3.3-.6.6-.6h2.5c.3 0 .6.3.6.6v6.8c0 .3-.3.6-.6.6H3.7zm16.1-5.6c-.2.2-.2.5-.1.7 0 0 .2.3.2.7 0 .5-.2 1-.8 1.4-.2.2-.3.4-.2.6 0 0 .2.6-.1 1.1-.3.5-.9.9-1.8 1.1-.8.2-1.8.2-3 .1h-.1c-2.7.1-5.4-.3-5.4-.3H8v-7.2c0-.2 0-.4-.1-.5.1-.3.3-.9.8-1.4 1.9-1.5 3.7-6.5 3.8-6.7v-.3c-.1-.5 0-1 .1-1.2.2 0 .8.1 1.2.6.4.6.4 1.6-.1 3-.7 2.1-.7 3.2-.2 3.7.3.2.6.3.9.2.3-.1.5-.1.7-.1h.1c1.3-.3 3.6-.5 4.4.3.7.6.2 1.4.1 1.5-.2.2-.1.5.1.7 0 0 .4.4.5 1 0 .3-.2.6-.5 1z"
+                    }
+                  })
+                ]
+              ),
+              _vm._v(" "),
+              _c("p", { staticClass: "ml-2" }, [_vm._v("讚")])
+            ]
+          ),
+          _vm._v(" "),
+          _c(
+            "button",
+            {
+              staticClass:
+                "flex justify-center py-1 rounded-lg text-sm text-gray-700 w-full hover:bg-gray-200"
+            },
+            [
+              _c(
+                "svg",
+                {
+                  staticClass: "fill-current w-5 h-5",
+                  attrs: {
+                    xmlns: "http://www.w3.org/2000/svg",
+                    viewBox: "0 0 24 24"
+                  }
+                },
+                [
+                  _c("path", {
+                    attrs: {
+                      d:
+                        "M20.3 2H3.7C2 2 .6 3.4.6 5.2v10.1c0 1.7 1.4 3.1 3.1 3.1V23l6.6-4.6h9.9c1.7 0 3.1-1.4 3.1-3.1V5.2c.1-1.8-1.3-3.2-3-3.2zm1.8 13.3c0 1-.8 1.8-1.8 1.8H9.9L5 20.4V17H3.7c-1 0-1.8-.8-1.8-1.8v-10c0-1 .8-1.8 1.8-1.8h16.5c1 0 1.8.8 1.8 1.8v10.1zM6.7 6.7h10.6V8H6.7V6.7zm0 2.9h10.6v1.3H6.7V9.6zm0 2.8h10.6v1.3H6.7v-1.3z"
+                    }
+                  })
+                ]
+              ),
+              _vm._v(" "),
+              _c("p", { staticClass: "ml-2" }, [_vm._v("留言")])
+            ]
+          )
+        ]
+      )
+    ]
+  )
 }
 var staticRenderFns = [
   function() {
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c(
-      "div",
-      { staticClass: "bg-white rounded shadow w-11/12 max-w-sm my-2" },
-      [
-        _c("div", { staticClass: "h-12 my-2 px-4" }, [
-          _c("div", { staticClass: "flex" }, [
-            _c("div", { staticClass: "mr-2" }, [
-              _c("img", {
-                staticClass:
-                  "w-10 h-10 object-cover rounded-full inline-block align-middle",
-                attrs: {
-                  src:
-                    "https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg",
-                  alt: "user image"
-                }
-              })
-            ]),
-            _vm._v(" "),
-            _c("div", [
-              _c("div", { staticClass: "text-base font-bold" }, [
-                _vm._v("\n          User Name\n        ")
-              ]),
-              _vm._v(" "),
-              _c("div", { staticClass: "text-xs text-gray-600" }, [
-                _vm._v("\n          12分鐘前\n        ")
-              ])
-            ])
+    return _c("div", { staticClass: "flex flex-col p-2" }, [
+      _c("div", { staticClass: "flex items-center" }, [
+        _c("div", [
+          _c("img", {
+            staticClass:
+              "w-10 h-10 object-cover rounded-full align-middle mr-2",
+            attrs: {
+              src:
+                "https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg",
+              alt: "user image"
+            }
+          })
+        ]),
+        _vm._v(" "),
+        _c("div", [
+          _c("div", { staticClass: "text-sm font-bold" }, [_vm._v("Username")]),
+          _vm._v(" "),
+          _c("div", { staticClass: "text-xs text-gray-600" }, [
+            _vm._v("12 分鐘前")
           ])
         ])
-      ]
-    )
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "mt-4" }, [_vm._v("\n      我的第一PO\n    ")])
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "w-full" }, [
+      _c("img", {
+        staticClass: "w-full",
+        attrs: {
+          src:
+            "https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          alt: "post image"
+        }
+      })
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", [_c("p", [_vm._v("123則留言")])])
   }
 ]
 render._withStripped = true
